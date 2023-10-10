5ec167d8c480d92ee71e5c1fec4acfec87c1962d
Michael He <97993031+michaelhe999@users.noreply.github.com>
2023-10-10 13:25:36 -0700

    Create SearchEngine.java

---
+++ SearchEngine.java
@@ -0,0 +1,49 @@
+import java.io.IOException;
+import java.net.URI;
+import java.util.ArrayList;
+
+class Handler implements URLHandler {
+    // The one bit of state on the server: a number that will be manipulated by
+    // various requests.
+    ArrayList<String> added = new ArrayList<String>();
+
+    public String handleRequest(URI url) {
+        if (url.getPath().equals("/add")) {
+            String[] parameters = url.getQuery().split("=");
+                if (parameters[0].equals("s")) {
+                    added.add(parameters[1]);
+                    return String.format("Added %s!", parameters[1]);
+                }
+            
+        } 
+        else {
+            if (url.getPath().contains("/search")) {
+                String[] parameters = url.getQuery().split("=");
+                if (parameters[0].equals("s")) {
+                    String returns = "";
+                    for(int i = 0; i < added.size(); i++){
+                        if(added.get(i).contains(parameters[1])){
+                            returns = returns + added.get(i) + "\n";
+                        }
+                    }
+                    return returns;
+                }
+            }
+            return "404 Not Found!";
+        }
+    return "404 Not Found!";
+    }
+}
+
+class SearchEngine {
+    public static void main(String[] args) throws IOException {
+        if(args.length == 0){
+            System.out.println("Missing port number! Try any number between 1024 to 49151");
+            return;
+        }
+
+        int port = Integer.parseInt(args[0]);
+
+        Server.start(port, new Handler());
+    }
+}
